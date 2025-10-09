9109c02937dcf52939b6515d35c460223ffc84cf
etammetar-cell <etammetar@hkhk.edu.ee>
2025-10-09 14:42:54 +0300

    Add files via upload

---
+++ Harjutus02.py
@@ -0,0 +1,47 @@
+import turtle
+
+#kilpkonna seaded
+turtle.speed(0)
+ekraan = turtle.Screen()
+ekraan.title("Olümpiarõngad - Erik")
+ekraan.setup(500,400)
+
+
+
+
+
+#olümpiarõngad
+turtle.pencolor("blue")
+turtle.pensize(6)
+turtle.penup()
+turtle.goto(-110,0)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.pencolor("black")
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.pencolor("red")
+turtle.penup()
+turtle.goto(110,0)
+turtle.pendown()
+turtle.circle(50)
+
+
+turtle.pencolor("yellow")
+turtle.penup()
+turtle.goto(-55,-45)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.pencolor("green")
+turtle.penup()
+turtle.goto(55,-45)
+turtle.pendown()
+turtle.circle(50)
+
+
+turtle.done()
